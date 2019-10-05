a42f7fea5d05ca2268736adc4531b100e5a77aac
Domeniqque Dylluar <domeniqque@hotmail.com>
2019-10-05 17:09:52 -0400

    chore: resolve relative paths

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,4 +28,11 @@ module.exports = {
     'react/prop-types': 'off',
     'no-param-reassign': 'off',
   },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
